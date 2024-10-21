c04a1070f0a92947a8949ab4b091d733908ea544
Towsif Mahabub <147875322+Towsifsnm@users.noreply.github.com>
2024-10-22 00:47:43 +0600

    added file

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './assets/Blogs/Blogs'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <Header></Header>
+      <Blogs></Blogs>
+       
+    </>
+  )
+}
+
+export default App
